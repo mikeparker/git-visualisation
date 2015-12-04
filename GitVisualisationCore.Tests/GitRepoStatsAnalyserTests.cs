2534329cafc4b11c8de49a0295190f9b041ba31c
michael.parker <michael.parker@grantadesign.com>
2015-12-04 17:54:43 +0000

    First commit

---
+++ GitVisualisationCore.Tests/GitRepoStatsAnalyserTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace GitVisualisationCore.Tests
+{
+    [TestFixture]
+    public class GitRepoStatsAnalyserTests
+    {
+        [Test]
+        public void TestCollectingCurrentFiles()
+        {
+            var sut = new GitRepoStatsAnalyser(@"C:\git\git-visualisation\");
+
+            var allCSharpFilePaths = sut.GetAllCSharpFilePaths();
+
+            foreach (var file in allCSharpFilePaths)
+            {
+                Debug.WriteLine(file);
+            }
+        }
+
+        [Test]
+        public void TestNothing()
+        {
+            Assert.That(8, Is.EqualTo(6+2));
+        }
+    }
+}
---
+++ GitVisualisationCore/GitRepoStatsAnalyser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GitVisualisationCore
+{
+    public class GitRepoStatsAnalyser
+    {
+        private readonly string baseFilePath;
+
+        public GitRepoStatsAnalyser(string baseFilePath)
+        {
+            this.baseFilePath = baseFilePath;
+        }
+
+        public IEnumerable<string> GetAllCSharpFilePaths()
+        {
+            return Directory.EnumerateFiles(baseFilePath, "*.cs", SearchOption.AllDirectories).Where(s => !s.Contains("TemporaryGeneratedFile"));
+        }
+    }
+}
